86c08d504e167f7568eb476712f1477d8764976b
dilki avishka <dilkiliyanaarachchi1@gmail.com>
2020-11-14 20:26:49 +0530

    designing diagrams

--- Tourism-Management-updated-master/src/main/java/com/restapi/groupwork/demo/dao/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restapi.groupwork.demo.dao;
-
-import com.restapi.groupwork.demo.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-}
--- Tourism-Management-updated-master/src/main/java/com/restapi/groupwork/demo/dao/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restapi.groupwork.demo.dao;
-
-import com.restapi.groupwork.demo.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
